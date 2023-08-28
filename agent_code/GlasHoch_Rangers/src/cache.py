8f37fadc2d435fd1f50f55011c92127c822f1495
Clemens Langer <clemens.langer@stud.uni-heidelberg.de>
2023-08-28 19:25:19 +0200

    add sample method to cache

--- agent_code/GlasHoch_Rangers/src/cache.py
+++ agent_code/GlasHoch_Rangers/src/cache.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Memory:
     def __init__(self, input_dim: tuple[int, int, int], size: int):
         self.size = size
@@ -10,7 +11,7 @@ class Memory:
         self.actions = np.zeros((size), dtype=np.str_)
         self.rewards = np.zeros((size), dtype=np.int32)
 
-    def cache(self, state: np.ndarray, next_state: np.ndarray, action: str, reward:int):
+    def cache(self, state: np.ndarray, next_state: np.ndarray, action: str, reward: int):
         if self.index >= self.size:
             self.index = 0
 
@@ -19,3 +20,7 @@ class Memory:
         self.actions[self.index] = action
         self.rewards[self.index] = reward
         self.index += 1
+
+    def sample(self, batch_size: int = 1) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        indices = np.random.choice(self.size, batch_size, replace=False)
+        return self.states[indices], self.next_states[indices], self.actions[indices], self.rewards[indices]
