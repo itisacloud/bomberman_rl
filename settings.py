c899c00f80660ab929c87a1db47ef29d726e9ba8
Clemens Langer <clemens.langer@stud.uni-heidelberg.de>
2023-09-10 23:30:42 +0200

    create crates in coin_heaven

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ SCENARIOS = {
         "COIN_COUNT": 0 
     },
     "coin-heaven": {
-        "CRATE_DENSITY": 0,
+        "CRATE_DENSITY": 0.05,
         "COIN_COUNT": 50
     },
     "loot-crate": { 
