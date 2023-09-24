855012a10d2316edceae8a2ff27394ebbd590a03
Clemens Langer <clemens.langer@stud.uni-heidelberg.de>
2023-09-24 10:09:01 +0200

    update 7th run

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task 2
 #SBATCH -p exercise
@@ -13,4 +13,4 @@ source ~/.bashrc
 conda activate bomberEnv
 
 echo "Running bomber training"
-python main.py play --agents GlasHoch_Rangers --train 1 --n-rounds 300000 --no-gui --scenario easy-Start
+python main.py play --agents GlasHoch_Rangers peaceful_agent random_agent --train 1 --n-rounds 300000 --no-gui
